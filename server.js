b7b439dc98659d1593d5a3b2126a0571254a3601
zackadoodledoo <sin12014@byui.edu>
2026-04-01 16:22:28 -0700

    server.js port fix for Render

--- server.js
+++ server.js
@@ -197,5 +197,5 @@ app.use(errorHandler);
 -------------------------------- */
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+   console.log(`Server running on port ${PORT}`);
 });
